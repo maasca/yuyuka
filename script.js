fb0643b6df112f99b4503acb9c8a396493799919
Louis Vivier <louis.vivier@hotmai.fr>
2022-09-29 16:53:09 +0200

    style à peu près fini. JQuery à faire

---
+++ script.js
@@ -0,0 +1,6 @@
+$("#donner").on("click", function() {
+  window.location.href =
+    "https://fr.openfoodfacts.org/faire-un-don-a-open-food-facts?utm_source=off&utm_medium=web&utm_campaign=donate-2022&utm_term=en-text-button";
+})
+
+
